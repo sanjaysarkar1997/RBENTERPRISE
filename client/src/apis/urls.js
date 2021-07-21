4d901b4c47bc989dbaf75e8a987267d34a8cdc0e
SANJAY SARKAR <sanjaysarkar430@gmail.com>
2021-07-22 00:31:33 +0530

    URL

--- client/src/apis/urls.js
+++ client/src/apis/urls.js
@@ -1,9 +1,9 @@
 const apis = {
-  baseUrl: "http://localhost:5000/api/v1/",
+  baseUrl: "https://rbenterprise.herokuapp.com/api/v1/",
   ADD_ITEM: "create-product",
   GET_ITEMS: "get-products",
   CREATE_BILL: "create-bill",
-  UPDATE_BILL: "update-bill",
+  UPDATE_BILL:"update-bill",
   VIEW_BILLS: "get-bills",
   VIEW_BILL: "get-bill",
   UPDATE_STOCK: "update-stock",
@@ -11,7 +11,7 @@ const apis = {
   UPDATE_ITEM: "update-item",
   DELETE_ITEM: "delete-item",
   GET_CUSTOMERS: "get-customers",
-  CREATE_CUSTOMER: "create-customer",
+  CREATE_CUSTOMER:"create-customer"
 };
 
 export default apis;
