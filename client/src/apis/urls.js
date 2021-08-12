0f284a96dd4295d2306b8fb0056145e9fa708be6
SANJAY SARKAR <sanjaysarkar430@gmail.com>
2021-08-12 17:34:08 +0530

    update

--- client/src/apis/urls.js
+++ client/src/apis/urls.js
@@ -2,7 +2,7 @@
 // https://rbenterprise.herokuapp.com
 
 const apis = {
-  baseUrl: "http://localhost:5000/api/v1/",
+  baseUrl: "https://rbenterprise.herokuapp.com/api/v1/",
   ADD_ITEM: "create-product",
   GET_ITEMS: "get-products",
   CREATE_BILL: "create-bill",
