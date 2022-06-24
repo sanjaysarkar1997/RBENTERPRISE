5bae23e378e853a6575542e1eb5d9827e664e478
SANJAY SARKAR <49182104+sanjaysarkar1997@users.noreply.github.com>
2022-06-24 13:14:32 +0530

    Remove Little Angel and Add Paree

--- client/src/services/colorGenerator.js
+++ client/src/services/colorGenerator.js
@@ -1,7 +1,6 @@
 let company = [
   { name: "Unicharm", color: "#673AB7" },
   { name: "Asian Paints", color: "#2196F3" },
-  { name: "Little Angel", color: "#4caf50" },
   { name: "Bajaj", color: "#801783" },
   { name: "Paree", color: "#42a7ff" },
 ];
